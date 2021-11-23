60d67ae1b23d0ceb3ed6512a66b3b181c9588eeb
David Silva <davidlsilva.sp@gmail.com>
2021-11-23 09:24:45 -0300

    Auth route

---
+++ api/auth.js
@@ -0,0 +1,13 @@
+const ImageKit = require("imagekit");
+
+const imagekit = new ImageKit({
+    urlEndpoint: process.env.REACT_APP_URL_END_POINT,
+    publicKey: process.env.REACT_APP_PUBLIC_KEY,
+    privateKey: process.env.REACT_APP_PRIVATE_KEY,
+});
+
+
+module.exports = app;module.exports = (req, res) => {
+    var result = imagekit.getAuthenticationParameters();
+    res.send(result);
+};
