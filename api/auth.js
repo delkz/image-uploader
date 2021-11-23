38c390ae3239a265db96d0f4b69a76031b02dc05
David Silva <davidlsilva.sp@gmail.com>
2021-11-23 10:04:29 -0300

    Cors Auth

--- api/auth.js
+++ api/auth.js
@@ -1,3 +1,15 @@
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+}
+  
 const ImageKit = require("imagekit");
 
 const imagekit = new ImageKit({
@@ -6,7 +18,9 @@ const imagekit = new ImageKit({
     privateKey: process.env.REACT_APP_PRIVATE_KEY,
 });
 
-module.exports = (req, res) => {
+const handler = (req, res) => {
     var result = imagekit.getAuthenticationParameters();
     res.send(result);
-};
+}
+  
+module.exports = allowCors(handler)
